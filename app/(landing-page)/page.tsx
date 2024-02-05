c5ff81e6bc7f555f9a3c853ce24ad27d489f9dc3
Jatesh Parikh <jateshparikh97@gmail.com>
2024-02-06 00:19:09 +0530

    Project setup

---
+++ app/(landing-page)/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/navbar/navbar";
+
+const LandingPage = () => {
+  return (
+    <section>
+      <Navbar />
+    </section>
+  );
+};
+
+export default LandingPage;
---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,11 @@
+import Logo from "./_components/logo";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <Logo />
+    </nav>
+  );
+};
+
+export default Navbar;
