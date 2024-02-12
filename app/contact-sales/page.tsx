54a118dba0cfd0025c267ac2100d0470e0828ef8
Jatesh Parikh <jateshparikh97@gmail.com>
2024-02-12 22:05:31 +0530

    Project completed

---
+++ app/contact-sales/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/navbar/navbar";
+import ContactForm from "./contact-form";
+import Footer from "@/components/footer";
+
+const Contact = () => {
+  return (
+    <>
+      <Navbar />
+      <ContactForm />
+      <Footer />
+    </>
+  );
+};
+
+export default Contact;
